7f76aadb9366ac3714a6fc0074f6ad4f6e12046f
Titobi <olumoyinjoshua@gmail.com>
2024-03-18 07:07:38 -0700

    advance api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,10 +7,11 @@ def number_of_subscribers(subreddit):
     """Returne totalnumber of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        'User-Agent': 'My User Agent 1.0'
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    if  response.status_code == 200:
+         results = response.json().get("data")
+         return results.get("subscribers")
